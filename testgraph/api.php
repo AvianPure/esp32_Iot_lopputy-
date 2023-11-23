dabeafaadf157a9fc62f455681973f3052267200
AvianPure <129159138+AvianPure@users.noreply.github.com>
2023-11-23 12:08:21 +0200

    modified:   readme.md 	modified:   testgraph/api.php 	modified:   testgraph/main.html

--- testgraph/api.php
+++ testgraph/api.php
@@ -4,13 +4,13 @@
 
 
   $servername = "localhost";
-  $username = "nodered";
-  $password = "node";
+  $username = "arduinoantti";
+  $password = "antti";
   $dbname = "dht22";
 
   $con = new PDO("mysql:host=$servername;dbname=$dbname",$username,$password);
   $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  $stmt = $con->prepare("SELECT AI AS ID, TempatureC FROM tempatures ORDER BY AI DESC LIMIT 100;");
+  $stmt = $con->prepare("SELECT AI, Timestapmp AS ID, TempatureC FROM tempatures ORDER BY AI DESC LIMIT 100;");
   $stmt->execute();
   $result = $stmt->fetchALL(PDO::FETCH_ASSOC);
   echo json_encode($result);
